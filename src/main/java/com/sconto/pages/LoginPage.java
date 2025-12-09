d671442f5fca5abd3fcf0382faf36f65e65b61b5
KristinaTomash <kristitomsh@gmail.com>
2025-12-09 12:53:43 +0200

    Add 2..2

--- src/main/java/com/sconto/pages/LoginPage.java
+++ src/main/java/com/sconto/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.sconto.pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.sconto.utils.PropertiesLoader;
 import io.cucumber.java.en_lol.WEN;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,6 +12,10 @@ import static com.codeborne.selenide.Selenide.$;
 
 
 public class LoginPage {
+
+    public static final String validEmail = PropertiesLoader.loadProperty("valid.email");
+    public static final String validPassword = PropertiesLoader.loadProperty("valid.password");
+    public static final String userName = PropertiesLoader.loadProperty("user.name");
 
     @FindBy(css = ".existingAccount__headline")
     WebElement loginTitle;
@@ -24,10 +29,11 @@ public class LoginPage {
     WebElement emailField;
     @FindBy(id = "loginPassword")
     WebElement passwordField;
-    public LoginPage enterData(String email, String password) {
 
-        $(emailField).val(email);
-        $(passwordField).val(password);
+    public LoginPage enterData() {
+
+        $(emailField).val(validEmail);
+        $(passwordField).val(validPassword);
 
         return Selenide.page(this);
     }
@@ -40,7 +46,8 @@ public class LoginPage {
     }
         @FindBy(css = ".titleHeadline")
         WebElement userNameTitle;
-    public SelenideElement verifyName(String name) {
-        return $(userNameTitle).shouldHave(text(name));
+
+    public SelenideElement verifyName() {
+        return $(userNameTitle).shouldHave(text(userName));
     }
 }
--- src/test/java/com/sconto/stepDefinitions/LoginSteps.java
+++ src/test/java/com/sconto/stepDefinitions/LoginSteps.java
@@ -14,7 +14,7 @@ public class LoginSteps {
     @And("User enters valid data")
     public void user_enters_valid_data(){
         login = Selenide.page(LoginPage.class);
-        login.enterData("manuel22@gmail.com","Aa12345!");
+        login.enterData();
     }
     @And("User click on Anmelden button")
     public void user_click_on_Anmelden_button(){
@@ -28,7 +28,7 @@ public class LoginSteps {
     }
     @Then("User verifies his name")
     public void verify_User_name(){
-        login.verifyName("Kristina Tomash");
+        login.verifyName();
     }
 
 }
